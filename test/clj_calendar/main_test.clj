4b84c20af9332af22380c562698a835ff81e79fa
Stuart Halloway <stu@thinkrelevance.com>
2009-09-14 13:17:19 -0400

    simple test automation

---
+++ test/clj_calendar/main_test.clj
@@ -0,0 +1,5 @@
+(ns clj-calendar.main-test
+  (:use clojure.test clj-calendar.main))
+
+(deftest succeeding
+  (is (= 1 1)))
---
+++ test/test.clj
@@ -0,0 +1,25 @@
+(use 'clojure.test)
+
+(defmacro re-test [test-sym]
+  `(do
+     (require :reload-all '~test-sym)
+     (clojure.contrib.test-is/run-tests '~test-sym)))
+
+(def tests
+     '[clj-calendar.main-test])
+
+(def exit-code (atom 0))
+
+(defn run-tests! [& args]
+  "Runs all tests"
+  (doseq [test tests] (require test))
+  (let [test-results (apply merge-with + (map test-ns tests))]
+
+    (if (or (< 0 (test-results :fail))
+            (< 0 (test-results :error)))
+      (reset! exit-code -1)))
+  (if (= [:shutdown-agents] args)
+    (shutdown-agents))
+  (System/exit @exit-code))
+
+
