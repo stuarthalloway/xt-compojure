69e8ed430475b8f28c8b520399658905834982c4
Stuart Halloway <stu@thinkrelevance.com>
2009-09-14 15:17:10 -0400

    tested html->clj against both core and contrib xml sequences

--- src/clj_calendar/main.clj
+++ src/clj_calendar/main.clj
@@ -1,6 +1,9 @@
 (ns clj-calendar.main
   (:use compojure))
 
+(defn calendar []
+  [:table {:class "calendar" :border 0 :cellspacing 0 :cellpadding 0}])
+
 (defn root []
   (html
    (doctype :html4)
@@ -9,8 +12,32 @@
      [:title "Calendar"]
     [:body
      [:div#container
-      [:h1 "TBD: draw a calendar"]]]]]))
+      [:h1 "TBD: draw a calendar"]
+      (calendar)]]]]))
 
 (defroutes clj-calendar-server
   (GET "/" (root))
   (ANY "*" (page-not-found)))
+
+(defn cdata? [node*]
+  (or (string? node*) (every? string? node*)))
+
+(defmulti html->clj #(cond
+                      (cdata? %) :cdata
+                      (sequential? %) :nodes
+                      :else :node))
+
+(defmethod html->clj :nodes [nodes]
+  (apply vector (map html->clj nodes)))
+
+(defmethod html->clj :cdata [node] node)
+
+(defmethod html->clj :node [node]
+  (vec
+   (let [attrs (node :attrs)]
+     (if (seq attrs)
+       (concat [(node :tag) attrs]
+               (html->clj (node :content)))
+       (cons (node :tag)
+             (html->clj (node :content)))))))
+
--- test/clj_calendar/main_test.clj
+++ test/clj_calendar/main_test.clj
@@ -1,5 +1,29 @@
 (ns clj-calendar.main-test
-  (:use clojure.test clj-calendar.main))
+  (:use clojure.test clj-calendar.main clojure.contrib.lazy-xml)
+  (:require [clojure.xml :as xml]))
 
-(deftest succeeding
-  (is (= 1 1)))
+(defn string->xml [str]
+  (xml/parse (org.xml.sax.InputSource. (java.io.StringReader. str))))
+
+(defn string->lazyxml [str]
+  (parse-trim (java.io.StringReader. str)))
+
+(deftest test-html->compojure
+  (doseq [[result input]
+           (partition 2 [
+                         [:h1 "hello"]
+                         "<h1>hello</h1>"
+       
+                         [:h1 {:class "friendly"} "howdy"]
+                         "<h1 class='friendly'>howdy</h1>"
+
+                         [:div {:id "chapter"} [:p "And so..."]]
+                         "<div id='chapter'><p>And so...</p></div>"
+
+                         [:div [:p "one"] [:p "two"]]
+                         "<div><p>one</p><p>two</p></div>"
+
+                         [:div "mixed" [:span "content"] "ftw"]
+                         "<div>mixed<span>content</span>ftw</div>"])]
+    (is (= result (html->clj (string->xml input))))
+    (is (= result (html->clj (string->lazyxml input))))))
